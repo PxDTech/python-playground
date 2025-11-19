d1431b937842a5cf916e7dd632fac3cd8813a5d6
PxDTech <118567236+PxDTech@users.noreply.github.com>
2025-11-19 09:03:51 -0500

    Add files via upload
    Codewars 8kyu kata
    
    Complete the solution so that it reverses the string passed into it.

---
+++ reversed_strings.py
@@ -0,0 +1,10 @@
+"""Complete the solution so that it reverses the string passed into it."""
+
+
+def reversed_strings(string):
+    return string[::-1]
+
+
+if __name__ == "__main__":
+    example = "word"
+    print(reversed_strings(example))
