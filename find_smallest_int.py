5eb3c17ab740498b0c2f44575275e413a9ff228a
PxDTech <118567236+PxDTech@users.noreply.github.com>
2026-07-16 16:18:16 -0400

    Initial commit
    Given an array of integers your solution should find the smallest integer.

---
+++ find_smallest_int.py
@@ -0,0 +1,9 @@
+# Given an array of integers your solution should find the smallest integer.
+
+
+def find_smallest_int(arr):
+    return min(arr)
+
+
+print(find_smallest_int([34, 15, 88, 2]))
+print(find_smallest_int([34, -345, -1, 100]))
