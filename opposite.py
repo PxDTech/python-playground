591bbc9f58a1766fb53b70d6229bbc3e3beb4de9
PxDTech <118567236+PxDTech@users.noreply.github.com>
2026-07-16 16:00:55 -0400

    Add files via upload
    # Very simple, given a number, find its opposite (additive inverse).

---
+++ opposite.py
@@ -0,0 +1,10 @@
+# Very simple, given a number, find its opposite (additive inverse).
+
+
+def opposite(number):
+    return -number
+
+
+print(opposite(1))
+print(opposite(14))
+print(opposite(-34))
